6ce02c163a236bf69cd0a309f9b884a98a8a693f
김영채 <kevinkim2586@gmail.com>
2021-02-19 12:35:55 +0900

    add and connect IBOutlets for AlbumCollectionViewCell

--- MyAlbum/Controller/AlbumViewController.swift
+++ MyAlbum/Controller/AlbumViewController.swift
@@ -3,8 +3,8 @@ import Photos
 
 class AlbumViewController: UIViewController {
     
+    @IBOutlet weak var albumCollectionView: UICollectionView!
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
        
---
+++ MyAlbum/View/AlbumCollectionViewCell.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class AlbumCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var albumImageView: UIImageView!
+    @IBOutlet weak var albumNameLabel: UILabel!
+    @IBOutlet weak var albumTotalPictureLabel: UILabel!
+    
+    
+}
